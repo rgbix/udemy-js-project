848e88fbb91c4cd3fc0434f5d4c67bb54625cc3f
rgbix <rgbixmail@gmail.com>
2020-11-16 20:49:28 +0300

    practice. part 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log(1);
+//exersize 1
+
+let numberOfFilm = prompt('Сколько фильмов вы уже посмотрели?', "");
+
+//exersize 2
+let personalMovieDB = {
+    count:numberOfFilm,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+
+//exersize 3
+
+let lastWathedFilm = prompt('Один из последних просмотренных фильмов?',''),
+    rating = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastWathedFilm] = rating;
+
+lastWathedFilm = prompt('Один из последних просмотренных фильмов?','');
+rating = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastWathedFilm] = rating;
+
+console.log(personalMovieDB);10
